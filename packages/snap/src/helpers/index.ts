6a8c7ed0258fc066b693fff7dccb991f96b105c3
Danil Dovgal <91081974+qweblessed@users.noreply.github.com>
2024-01-30 23:14:57 +0100

    feat: wrong chain edge case

--- packages/snap/src/helpers/index.ts
+++ packages/snap/src/helpers/index.ts
@@ -1,7 +1,4 @@
-import { ethers } from 'ethers';
-import { ENS } from '@ensdomains/ensjs';
 import { createClient } from '@supabase/supabase-js';
-import abi from '../../abi/PointsDistributor.json';
 
 export const validateShortcut = (shortcuts: any, to: string) => {
   return shortcuts.find(
@@ -9,27 +6,24 @@ export const validateShortcut = (shortcuts: any, to: string) => {
   );
 };
 
-const getProvider = () => {
-  return new ethers.providers.JsonRpcProvider(
-    `https://eth-mainnet.g.alchemy.com/v2/H0EPsQJ2P_G89TbtjWux_6AJI1WR5pUr`,
-  );
-};
-
 export const createSupabaseClient = async () => {
   return await createClient(
-    'https://vmcqqamkkquopdyzdxnv.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtY3FxYW1ra3F1b3BkeXpkeG52Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA3NjM5NTMsImV4cCI6MjAxNjMzOTk1M30.tDhro3SGZw6SbB5nxJxjPRrSgJE6oP0XZoqtDntQRI4',
+    'https://ogofkfgmjjhakgojhhiy.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9nb2ZrZmdtampoYWtnb2poaGl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIzMjA2NzUsImV4cCI6MjAxNzg5NjY3NX0.ebyMLVX98bSCxBxMB2JGAQqF4h0XBEQzmBsCwM71rgg',
   );
 };
 
-export const estimateRewardPoints = async (address: any, value: any) => {
-  const pointsContract = new ethers.Contract(
-    '0x60298a0A28df3a9fae38dd7f232144C8497c1fE1',
-    abi,
-    getProvider(),
-  );
-  
-  const shortcut = await pointsContract.shortcuts(address);
-  
-  return await pointsContract.calculatePoints(value, shortcut.complexity);
+export const estimateRewardPoints = async (
+  address: any,
+  value: any,
+  validatedShortcutData: any,
+  supabase: any,
+) => {
+  const { data: stage } = await supabase
+    .from('points_distribution_state')
+    .select('*');
+
+  return stage.length
+    ? (value * validatedShortcutData.complexity * stage[0].stage_multiplier / 10**19).toFixed(0)
+    : 0;
 };
--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,26 +1,36 @@
 import { OnTransactionHandler } from '@metamask/snaps-types';
-import { heading, panel, text, divider } from '@metamask/snaps-ui';
+import { panel, text, divider } from '@metamask/snaps-ui';
 import {
   createSupabaseClient,
   estimateRewardPoints,
   validateShortcut,
 } from './helpers';
 
-export const onTransaction: OnTransactionHandler = async ({ transaction }) => {
-  console.log('transactionTo:', transaction?.to);
+export const onTransaction: OnTransactionHandler = async ({ transaction, chainId }) => {
   const supabase = await createSupabaseClient();
-
-  const { data: shortcuts } = await supabase.from('shortcuts_info').select('*');
-  console.log('q123');
+  const { data: shortcuts } = await supabase.from('shortcuts').select('*');
   const validatedShortcutData = validateShortcut(
     shortcuts,
     transaction.to as string,
   );
-  console.log('q123');
+  
+  if(chainId !== '1') {
+    return {
+      content: panel([
+        divider(),
+        text(`Shortcuts availabe ONLY`),        
+        text(`for MAINNET addresses`),
+        divider(),
+      ]),
+    };
+  }
+
   if (validatedShortcutData) {
     const estimatedPoints = await estimateRewardPoints(
       transaction?.to,
-      transaction?.value,
+      transaction?.value,      
+      validatedShortcutData,
+      supabase
     );
     return {
       content: panel([
@@ -28,16 +38,16 @@ export const onTransaction: OnTransactionHandler = async ({ transaction }) => {
         divider(),
         text(`Contract: ${validatedShortcutData.address}`),
         divider(),
-        text(`Verified by ONTHIS ✅`),
+        text(`Verified by ONTHIS ✅`),        
         divider(),
-        text(`Est points earning: ${(estimatedPoints[0] / 10 ** 18).toFixed(2)}`),
+        text(`Est. Points Receive: ${estimatedPoints}`)
       ]),
     };
   }
 
   return {
     content: panel([
-      text(`CONTRACT ${transaction?.to} IS NOT VERIFIED BY ONTHIS ❌`),
+      text(`CONTRACT ${transaction?.to} IS NOT VERIFI12ED BY ONTHIS ❌`),
       divider(),
       text(`Visit https://onthis.xyz/shortcuts`),
       text(`To see all verified shortcuts`),
